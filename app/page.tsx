5e0b88a1542fa70820bad363bf627dd57fbae173
SutPi0154 <sutpi070@gmail.com>
2024-10-01 12:04:54 +0630

    card-wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,17 @@ const font = Poppins({
 });
 export default function Home() {
   return (
-    <main className=" flex flex-col h-full justify-center items-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-100 to-blue-800">
-      <div className="space-y-6 ">
-        <h1 className=" text-6xl font-semibold text-white drop-shadow-md">
+    <main className=" flex flex-col h-full justify-center items-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 flex flex-col text-center">
+        <h1
+          className={cn(
+            " text-6xl m-0 font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
           Auth
         </h1>
-        <p className={cn(" text-white text-lg", font.className)}>
+        <p className={cn(" text-white text-lg mt-0 mb-3", font.className)}>
           A simple authentication service{" "}
         </p>
         <LoginButton>
