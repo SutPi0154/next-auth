c86d0672d651569a61bcab01f44d13b274a9e8a5
SutPi0154 <sutpi070@gmail.com>
2024-10-01 20:43:44 +0630

    login-register-ui

---
+++ schemas/index.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "email is required ",
+  }),
+  password: z.string().min(1, {
+    message: "password is required",
+  }),
+});
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "email is required ",
+  }),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters",
+  }),
+  name: z.string().min(4, {
+    message: "Name is required",
+  }),
+});
