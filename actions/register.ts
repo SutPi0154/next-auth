eacd6f036ea48f402c9cdc956a5c5664f86fec0c
SutPi0154 <sutpi070@gmail.com>
2024-10-02 15:56:02 +0630

    create-user done

--- actions/register.ts
+++ actions/register.ts
@@ -1,9 +1,28 @@
 "use server";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
 import * as z from "zod";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  const validatedField = RegisterSchema.safeParse(values);
-  if (!validatedField.success) return { error: "Invalid fields!" };
-  return { success: "Email sent!" };
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) return { error: "Invalid fields!" };
+
+  const { email, password, name } = validatedFields.data;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) return { error: "email already in use" };
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+    },
+  });
+  // TODO sent verification token to email
+  return { success: "User created !" };
 };
