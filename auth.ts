11f342db249e1b1770779c817d85302e36962eb8
SutPi0154 <sutpi070@gmail.com>
2024-10-07 11:07:35 +0630

    userRole done

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { User, UserRole } from "@prisma/client";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
 import { db } from "./lib/db";
 
 export const {
@@ -9,6 +11,38 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = (await getUserById(user.id as string)) as User;
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
+    async session({ session, token }) {
+      console.log("session =", session, "token =", token);
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      console.log(token.sub);
+      const existingUser = (await getUserById(token.sub)) as User | null;
+      console.log(existingUser);
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
--- data/user.ts
+++ data/user.ts
@@ -11,7 +11,7 @@ export const getUserByEmail = async (email: string) => {
 
 export const getUserById = async (id: string) => {
   try {
-    const user = await db.user.findUnique({ where: { id } });
+    return await db.user.findUnique({ where: { id } });
   } catch (error) {
     return null;
   }
